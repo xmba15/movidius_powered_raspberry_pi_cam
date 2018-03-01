a896338b1ac64d38c625c433b0325933a691f417
bajsk <tran@jsk.imi.i.u-tokyo.ac.jp>
2018-03-01 17:15:13 +0900

    add config script

---
+++ scripts/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+directory_root = os.path.dirname(os.path.realpath(__file__)) + "/../"
+data_path = os.path.join(directory_root, "data")
+model_path = os.path.join(directory_root, "models")
+image_path = os.path.join(directory_root, "images")
+
+class Config():
+
+    model_dir = model_path
+    image_dir = image_path
+
+    
