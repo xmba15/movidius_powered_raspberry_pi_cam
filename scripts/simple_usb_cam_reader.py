3eeb6d197190ebbd1a962775443279f11e97da92
xmba15 <tran@jsk.imi.i.u-tokyo.ac.jp>
2018-03-09 06:49:56 +0000

    mod line format

--- scripts/simple_usb_cam_reader.py
+++ scripts/simple_usb_cam_reader.py
@@ -10,12 +10,12 @@ def main(arg):
     argc = len(arg)
     if (argc > 1):
         cam_index = int(arg[1])
-    
+
     cap = cv2.VideoCapture(cam_index)
     while(True):
         ret, frame = cap.read()
         cv2.imshow("current_frame", frame)
-        
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
